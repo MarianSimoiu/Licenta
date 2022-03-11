2ed27d9c057b116e8c08ab1be62f5321ef98263a
MarianSimoiu <simoiumarian2000@yahoo.com>
2022-03-11 13:35:10 +0200

    vertical menu

--- Safe_Office/frontend/src/screens/MyNotes/MyNotes.js
+++ Safe_Office/frontend/src/screens/MyNotes/MyNotes.js
@@ -4,6 +4,8 @@ import MainScreen from "../../components/MainScreen";
 import { Link } from "react-router-dom";
 import ReactMarkdown from "react-markdown";
 
+import "./MyNotes.css";
+
 import { useDispatch, useSelector } from "react-redux";
 import { deleteNoteAction, listNotes } from "../../actions/notesActions";
 import Loading from "../../components/Loading";
@@ -56,82 +58,21 @@ function MyNotes({ history, search }) {
   };
 
   return (
-    <MainScreen title={`Welcome Back ${userInfo && userInfo.name}..`}>
-      {console.log(notes)}
-      <Link to="/createnote">
-        <Button style={{ marginLeft: 10, marginBottom: 6 }} size="lg">
-          Create new Note
-        </Button>
-      </Link>
-      {error && <ErrorMessage variant="danger">{error}</ErrorMessage>}
-      {errorDelete && (
-        <ErrorMessage variant="danger">{errorDelete}</ErrorMessage>
-      )}
-      {loading && <Loading />}
-      {loadingDelete && <Loading />}
-      {notes &&
-        notes
-          .filter((filteredNote) =>
-            filteredNote.title.toLowerCase().includes(search.toLowerCase())
-          )
-          .reverse()
-          .map((note) => (
-            <Accordion>
-              <Card style={{ margin: 10 }} key={note._id}>
-                <Card.Header style={{ display: "flex" }}>
-                  <span
-                    // onClick={() => ModelShow(note)}
-                    style={{
-                      color: "black",
-                      textDecoration: "none",
-                      flex: 1,
-                      cursor: "pointer",
-                      alignSelf: "center",
-                      fontSize: 18,
-                    }}
-                  >
-                    <Accordion.Toggle
-                      as={Card.Text}
-                      variant="link"
-                      eventKey="0"
-                    >
-                      {note.title}
-                    </Accordion.Toggle>
-                  </span>
-
-                  <div>
-                    <Button href={`/note/${note._id}`}>Edit</Button>
-                    <Button
-                      variant="danger"
-                      className="mx-2"
-                      onClick={() => deleteHandler(note._id)}
-                    >
-                      Delete
-                    </Button>
-                  </div>
-                </Card.Header>
-                <Accordion.Collapse eventKey="0">
-                  <Card.Body>
-                    <h4>
-                      <Badge variant="success">
-                        Category - {note.category}
-                      </Badge>
-                    </h4>
-                    <blockquote className="blockquote mb-0">
-                      <ReactMarkdown>{note.content}</ReactMarkdown>
-                      <footer className="blockquote-footer">
-                        Created on{" "}
-                        <cite title="Source Title">
-                          {note.createdAt.substring(0, 10)}
-                        </cite>
-                      </footer>
-                    </blockquote>
-                  </Card.Body>
-                </Accordion.Collapse>
-              </Card>
-            </Accordion>
-          ))}
-    </MainScreen>
+  <div>
+    <div class="sidebar">
+    <a class="active" href="#home">Home</a>
+    <a href="#news">News</a>
+    <a href="#contact">Contact</a>
+    <a href="#about">About</a>
+    </div>
+  
+    <div class="content">
+      <h2>Responsive Sidebar Example</h2>
+      <p>This example use media queries to transform the sidebar to a top navigation bar when the screen size is 700px or less.</p>
+      <p>We have also added a media query for screens that are 400px or less, which will vertically stack and center the navigation links.</p>
+      <h3>Resize the browser window to see the effect.</h3>
+    </div>
+  </div>
   );
 }
 
